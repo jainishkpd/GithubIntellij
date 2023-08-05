23f5177bde6750c3f911c43a6bdc806827dc1333
Jainish Kapadia <jainishkpd@gmail.com>
2023-08-05 13:58:11 +0530

    update name

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.net.StandardSocketOptions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kapadia");
-        System.out.println("Jainish");
-        System.out.println("Riyansh");
+        System.out.println("Hello world");
     }
 }
