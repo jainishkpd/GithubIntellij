f531de36f6c6b508318c3c73a614cb7bba760070
Jainish Kapadia <jainishkpd@gmail.com>
2023-08-05 13:41:54 +0530

    Change name

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.net.StandardSocketOptions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Kapadia");
         System.out.println("Jainish");
     }
 }
