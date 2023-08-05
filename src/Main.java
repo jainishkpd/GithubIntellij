47f91b70111ba1b329bfa488c4ca219776121dcb
Jainish Kapadia <jainishkpd@gmail.com>
2023-08-05 13:43:43 +0530

    Added name

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kapadia");
         System.out.println("Jainish");
+        System.out.println("Riyansh");
     }
 }
